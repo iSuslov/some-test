0204366aec082e95bde9094f5c9c34abef72cc08
Ivan Suslov <digitalzuzel@gmail.com>
2021-11-14 23:27:39 -0500

    test failed test

--- tests/testMainGreetings.test.js
+++ tests/testMainGreetings.test.js
@@ -2,5 +2,5 @@ const sayGreetings = require("../services/mainService");
 
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sayGreetings('Ivan')).toBe('Hello Ivan!');
+  expect(sayGreetings('Ivan')).toBe('Hello Ivan!!');
 });
